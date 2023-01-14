1f363fd20c103cb878816622155a5a7c80df1840
Dipta-Sarker <contact.diptasarker@gmail.com>
2023-01-14 16:15:21 +0600

    category api done

--- index.js
+++ index.js
@@ -28,6 +28,13 @@ async function run() {
             const result = await laptopsCollection.find(query).toArray()
             res.send(result)
         })
+        app.get('/laptops/:id', async(req,res)=>{
+            const id = req.params.id
+            console.log(id)
+            const query ={_id:ObjectId(id)}
+            const result = await laptopsCollection.find(query).toArray()
+            res.send(result)
+        })
 
         
 
